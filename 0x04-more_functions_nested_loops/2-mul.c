84be01e4420d80a09a3df912c8850d1eb7f1a548
Redoxm <luqmanaremu4@gmail.com>
2022-12-15 06:39:59 -0800

    ggggh

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - return multiplication of a * b
+ * @a: first number to multiply
+ * @b: secondary number to multiply
+ * Return: multiplication of a * b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - return 0-9
+ */
+
+void print_numbers(void)
+{
+int i;
+
+for (i = '0'; i <= '9'; i++)
+{
+_putchar(i);
+}
+
+_putchar('\n');
+}
